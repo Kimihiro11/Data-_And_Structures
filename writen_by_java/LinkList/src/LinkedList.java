820460045f052c9e84587a27465d18251b978bd0
kimhiro <cjassassinli1125@gmail.com>
2019-11-25 17:52:44 +0800

    feat: java part  linked list init

---
+++ writen_by_java/LinkList/src/LinkedList.java
@@ -0,0 +1,52 @@
+public class LinkedList<E> {
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illengal index.");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+}
