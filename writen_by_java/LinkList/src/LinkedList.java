00ddcc452ed77292dfaea16a06cf18563d35ad2e
kimhiro <cjassassinli1125@gmail.com>
2019-11-27 18:01:41 +0800

    feat: java part  linked list init

--- writen_by_java/LinkList/src/LinkedList.java
+++ writen_by_java/LinkList/src/LinkedList.java
@@ -49,4 +49,51 @@ public class LinkedList<E> {
         prev.next = new Node(e, prev.next);
         size++;
     }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed,illegal index");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed,illegal index");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
 }
